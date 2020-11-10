9a9f440a5e174bca8e9618c9bf2fceca812ea97d
Josh Haughton <Josh@CumberlandApartments.ca>
2020-11-09 23:40:53 -0700

    got basic functionality working now

--- app.js
+++ app.js
@@ -20,125 +20,59 @@ global.CD = {
 }
 
 
-
     function setupSockets(expressServer){
         this.io = require('socket.io')(expressServer);
-
         
         const controllerDataNsp = this.io.of('/controllerData');
         ((io)=>{
-            global.nsp = io;  //for interacting with the namespace from consel.
 
-            const m = require('socket.io-p2p-server');
-            
-            global.p2pserver = m.Server;
-            global.p2pclients = m.clients
-
-            global.rooms = [];
-            global.clients = {};
-
-            console.log('rooms at setup: ', + io.adapter.rooms.size);
+            const p2p = require('socket.io-p2p-server');
+            global.p2pserver = p2p.Server;
+            global.p2pclients = p2p.clients;
 
             io.on('connection', (socket)=>{
-                console.log('room count on connection start ', socket.id)
-                console.log(io.adapter.rooms.size);
-                for (let [key, value] of io.adapter.rooms) {
-                   console.log(value)
-                }
                 
-                socket.to('some room').on('message',(data)=>{console.log(data);});
+                function joinRoom(roomName){
+                    socket.join(roomName);
+                    p2pserver(socket, null, {name:roomName});
+                    socket.emit('roomName', roomName);
+                    
+                    socket.on('message', function (data) {
+                        socket.in(roomName).emit('message', data);
+                        console.log('message from room: ', roomName);
+                        console.log('message: ', data);
+                    });
 
+                    socket.on('goPrivate', function () {
+                        socket.in(roomName).emit('goPrivate');
+                    })
 
+                    console.log('rooms after join function');
+                    console.log(io.adapter.rooms);
+                }
 
-
-                clients[socket.id] = socket
-                var room = findOrCreateRoom()
-                socket.join(room.name);
-                //sending to all clients in the room INCLUDING the sender..
-                //io.in(room.name).emit('message','you are now in room ' + room.name);
-                socket.emit('message','you are now in room ' + room.name);
-                room.playerCount++
-                room.players.push(socket)
+                socket.on("joinRoom",(roomName)=>{
+                    joinRoom(roomName);
+                });
+                
+                socket.on('createRoom',()=>{
+                    const roomName = create_UUID();
+                    joinRoom(roomName);
+                })
 
                 socket.on('error', function (err) {
                     console.log("Error %s", err);
                 })
 
-                p2pserver(socket, null, room);
-                
-
-                socket.on('disconnect', function () {
-                    room.players.splice(room.players.indexOf(socket), 1)
-                    removeRoom(room)
-                    io.to(room.name).emit('message',"user disconnected...");
-
-                    // Move opponents to new rooms
-                    // var opponents = findOpponents(room.name, socket)
-                    console.log('user disconnected. ', socket.id);
-                    Object.keys(room.players).forEach(function (clientId, i) {
-                        room = findReadyRoom()
-                        if (clients[clientId] && room) {
-                            clients[clientId].join(room.name)
-                        }
-                    })
+                socket.on('disconnect', ()=>{
+                    console.log('client disconnected..');
                     console.log('rooms after disconnect');
                     console.log(io.adapter.rooms);
                 })
 
-                socket.on('message', function (data) {
-                    var players = room.players.filter(function (player) {
-                        return player !== socket
-                    })
-                    players.forEach(function (player) {
-                        player.emit('message', data);
-                        console.log('message from room: ', socket.room);
-                        console.log('message: ', data);
-                    })
-                })
-
-                    console.log('rooms after connect');
-                    console.log(io.adapter.rooms);
-
-                if (room.playerCount === 1) {
-                    console.log("Waiting for someone to join..");
-                    socket.emit('message','waiting for someone to join..')
-                } else {
-                    //sending to all clients in the room INCLUDING the sender..
-                    io.in(room.name).emit('message', 'This should be everone in the room...');
-                
-                
-                }
+                console.log('rooms after connect');
+                console.log(io.adapter.rooms);
             })
-
-            function findOrCreateRoom () {
-            var lastRoom = findReadyRoom ()
-            if (!lastRoom || lastRoom.full) {
-                var room = {players: [], playerCount: 0, name: create_UUID()}
-                return addRoom(room)
-            }
-            return lastRoom
-            }
-
-            function findReadyRoom () {
-            return rooms.filter(function(room) { return room.playerCount === 1 })[0];
-            }
-
-            function removeRoom (room) {
-            room.playerCount--
-            if (room.playerCount === 0) rooms.splice(rooms.indexOf(room), 1)
-            }
-
-            function addRoom (room) {
-            return rooms[rooms.push(room) - 1]
-            }
-
-
-
-
-
-
-
-
 
 
         })(controllerDataNsp);
